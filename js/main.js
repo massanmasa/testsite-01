8db57739feebc1ff1749f6f3fc3b0354608887f4
masamasa_uu <akatsukiryu848@gmail.com>
2018-08-06 10:29:10 +0900

    correction

--- js/main.js
+++ js/main.js
@@ -1,49 +1,5 @@
 let pointY;
 
-// localstorage使用の宣言
-// let storage = localStorage;
-
-// localStorage用配列
-// let arrayId = [];
-
-// localstorageがブラウザで使えるか判定する関数
-// const lsCheck = () => {
-//     try {
-//         if (typeof localStorage == 'undefined') {
-//             return false;
-//         } else if (window.localStorage) {
-//             // detect IE10 and private mode
-//         }
-//     } catch (e) {
-//         return false;
-//     }
-//     return true;
-// }
-
-// localstorageにデータを保存させる関数
-// const saveStorage = () => {
-//     let id = $('.favorite-on').parents('article').attr('id');
-//     arrayId.push(id);
-//     let data = JSON.stringify(arrayId);
-//     storage.setItem('newsId', data);
-//     console.log(storage);
-// }
-
-// localstorageのデータ取得、並びに処理
-// const loadStorage = () => {
-//     if (storage.length === 0) {
-//         return;
-//     }
-//     // arrayId.push(storage);
-//     let setId  = JSON.parse(storage.getItem('newsId'));
-//     for (let i = 0; i < setId[i]; i++) {
-//         // if($('article').attr('id') === setId[i]) {
-//             $('#' + setId[i]).find('i').addClass('favorite-on');
-//             console.log('#' + setId[i]);
-//         // }
-//     }
-// }
-
 // モーダルをセンタリングする関数
 const centeringModal = () => {
     let w = $(window).width();
@@ -69,14 +25,6 @@ const releaseScrolling = () => {
 }
 
 jQuery(function($) {
-
-    // localstorage使用できるか判定
-    // lsCheck();
-
-    // localstorageの中身をページに反映させる
-    // loadStorage();
-
-    // 200px以上スクロールされた場合、ページトップボタンを表示する
     $(window).on('scroll', function() {
         if ($(this).scrollTop() >= 200) {
             $('#pageTopBtn').fadeIn();
@@ -137,10 +85,5 @@ jQuery(function($) {
 
     $('.favorite').on('click', function() {
         $(this).toggleClass('favorite-on');
-        // if ($(this).hasClass('favorite-on')) {
-        //     saveStorage();
-        // } else {
-        //     storage.removeItem('newsId');
-        // }
     });
 });
